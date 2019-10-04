1c0a4879076b08541fcc079476695ddef56dc5d3
astlive <irandomsky@gmail.com>
2019-10-04 11:08:45 +0800

    add logger

--- server.py
+++ server.py
@@ -1,4 +1,21 @@
 import os
+import datetime
+import logging
+
+def initlog():
+    logger = logging.getLogger()
+    logger.setLevel('DEBUG')
+    log_format="[%(asctime)s]\t[%(levelname)s]\t%(message)s"
+    date_format = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter(log_format, date_format)
+    verb = logging.StreamHandler()
+    verb.setFormatter(formatter)
+    verb.setLevel('DEBUG')
+    logger.addHandler(verb)
+    fhlr = logging.FileHandler(datetime.datetime.now().strftime("log/tk%Y-%m-%d_%H_%M_%S.log"))
+    fhlr.setFormatter(formatter)
+    logger.addHandler(fhlr)
+    return logger
 
 def main():
     print("Remake Version:0.1")
