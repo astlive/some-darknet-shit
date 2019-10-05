889bc2cde3e339a5a4458c40c3b8143cbc1aa623
astlive <irandomsky@gmail.com>
2019-10-05 11:12:05 +0800

    getjob_1005

--- server.py
+++ server.py
@@ -2,10 +2,32 @@
 import os
 import datetime
 import logging
+import multiprocessing as mp
 
 #local module
 from thirdc import getpoints
 import dbcc
+
+class Server:
+    def __init__(self, darknet, logger, dbcc):
+        self.detector = darknet
+        self.logger = logger
+        self.db = dbcc
+        self.jobs = mp.Queue()
+    
+    def porter(self):
+        sql = 'SELECT * FROM file WHERE file.status = 0'
+        rr = self.db.query(sql)
+        self.logger.info("Query the undetect record from DB num:" + str(len(rr)))
+        for r in rr:
+            self.logger.debug("r in rr-->" + str(r))
+            job = {'fid':r[0],'fpath':r[2]}
+            self.logger.debug("job-->" + str(job))
+            self.jobs.put(job)
+            
+    def run(self):
+        mpPorter = mp.Process(target=self.porter,args=(self,))
+        mpPorter.start()
 
 def initlog():
     logger = logging.getLogger()
@@ -22,13 +44,12 @@ def initlog():
     logger.addHandler(fhlr)
     return logger
 
-def checker(logger):
+def checker(logger, dbcc):
     logger.info("checking darknet-detector")
-    logger.info("checking DB connecter") # Here to put your DB conn
-    db = dbcc()
-    isconn, ping = db.chk_db()
+    logger.info("checking DB connecter") # Here to put your DB conn function
+    isconn = dbcc.chk_db()
     if(isconn):
-        logger.info("Database Connected Ping: ", ping)
+        logger.info("Database Connected")
     else:
         logger.error("Database Connect Error")
         return False
@@ -39,11 +60,16 @@ def checker(logger):
     else:
         logger.error("GPX module failed")
         return False
+    return True
         
 def main():
     logger = initlog()
-    logger.info("Tra Darknet Service start")
-    
+    db = dbcc.dbcc()
+    logger.info("init the tra detector server")
+    if(checker(logger, db)):
+        srv = Server("darknet", logger, db)
+        logger.info("Server Start")
+        srv.run()
 
 if __name__ == '__main__':
     main()
